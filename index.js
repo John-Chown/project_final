da219271a55851b15bc36014691c1b96f0d69380
john-chown <johnchown@oakland.edu>
2021-12-08 22:09:33 -0500

    added serverside to quiz pages

--- index.js
+++ index.js
@@ -42,6 +42,268 @@ app.get("/quiz4", (req, res) => {
     res.render("../views/html/quiz4");
 });
 
+app.post("/", (req, res) => {
+    var score = 0;
+
+    if (req.body.q1Answer1 == "c"){
+        score++;
+    }
+
+    if (req.body.q1Answer2 == "d"){
+        score++;
+    }
+
+    if (req.body.q1Answer3 == "a"){
+        score++;
+    }
+
+    if (req.body.q1Answer4 == "c"){
+        score++;
+    }
+
+    if (req.body.q1Answer5 == "b"){
+        score++;
+    }
+
+    if (req.body.q1Answer6 == "b"){
+        score++;
+    }
+
+    if (req.body.q1Answer7 == "d"){
+        score++;
+    }
+
+    if (req.body.q1Answer8 == "b"){
+        score++;
+    }
+
+    if (req.body.q1Answer9 == "d"){
+        score++;
+    }
+
+    if (req.body.q1Answer10 == "a"){
+        score++;
+    }
+
+    console.log(score);
+
+
+    var score2 = 0;
+
+    if (req.body.q2Answer1 == "b"){
+        score2++;
+    }
+
+    if (req.body.q2Answer2 == "d"){
+        score2++;
+    }
+
+    if (req.body.q2Answer3 == "a"){
+        score2++;
+    }
+
+    if (req.body.q2Answer4 == "d"){
+        score2++;
+    }
+
+    if (req.body.q2Answer5 == "c"){
+        score2++;
+    }
+
+    if (req.body.q2Answer6 == "d"){
+        score2++;
+    }
+
+    if (req.body.q2Answer7 == "a"){
+        score2++;
+    }
+
+    if (req.body.q2Answer8 == "b"){
+        score2++;
+    }
+
+    if (req.body.q2Answer9 == "d"){
+        score2++;
+    }
+
+    if (req.body.q2Answer10 == "a"){
+        score2++;
+    }
+
+    console.log(score2);
+
+
+
+    var score3 = 0;
+
+    if (req.body.q3Answer1 == "a"){
+        score3++;
+    }
+
+    if (req.body.q3Answer2 == "c"){
+        score3++;
+    }
+
+    if (req.body.q3Answer3 == "b"){
+        score3++;
+    }
+
+    if (req.body.q3Answer4 == "c"){
+        score3++;
+    }
+
+    if (req.body.q3Answer5 == "d"){
+        score3++;
+    }
+
+    if (req.body.q3Answer6 == "a"){
+        score3++;
+    }
+
+    if (req.body.q3Answer7 == "c"){
+        score3++;
+    }
+
+    if (req.body.q3Answer8 == "d"){
+        score3++;
+    }
+
+    if (req.body.q3Answer9 == "c"){
+        score3++;
+    }
+
+    if (req.body.q3Answer10 == "b"){
+        score3++;
+    }
+
+    if (req.body.wqq1 == "Peter Parker"){
+        score3++;
+    }
+
+    if (req.body.wqq2 == "Ian Malcolm"){
+        score3++;
+    }
+
+    if (req.body.wqq3 == "Martin Brody"){
+        score3++;
+    }
+
+    if (req.body.wqq4 == "Frodo Baggins"){
+        score3++;
+    }
+
+    if (req.body.wqq5 == "Jules Winnfield"){
+        score3++;
+    }
+
+    if (req.body.wqq6 == "Dale Doback"){
+        score3++;
+    }
+
+    if (req.body.wqq7 == "Joker"){
+        score3++;
+    }
+
+    if (req.body.wqq8 == "John Wick"){
+        score3++;
+    }
+
+    if (req.body.wqq9 == "Woody"){
+        score3++;
+    }
+
+    if (req.body.wqq10 == "Uncle Bobby B"){
+        score3++;
+    }
+
+    console.log(score3);
+
+    var score4 = 0;
+
+    if (req.body.q4Answer1 == "a"){
+        score4++;
+    }
+
+    if (req.body.q4Answer2 == "c"){
+        score4++;
+    }
+
+    if (req.body.q4Answer3 == "d"){
+        score4++;
+    }
+
+    if (req.body.q4Answer4 == "a"){
+        score4++;
+    }
+
+    if (req.body.q4Answer5 == "d"){
+        score4++;
+    }
+
+    if (req.body.q4Answer6 == "d"){
+        score4++;
+    }
+
+    if (req.body.q4Answer7 == "a"){
+        score4++;
+    }
+
+    if (req.body.q4Answer8 == "d"){
+        score4++;
+    }
+
+    if (req.body.q4Answer9 == "b"){
+        score4++;
+    }
+
+    if (req.body.q4Answer10 == "c"){
+        score4++;
+    }
+
+    if (req.body.wq1 == "Get a coin"){
+        score4++;
+    }
+
+    if (req.body.wq2 == "Game over"){
+        score4++;
+    }
+
+    if (req.body.wq3 == "Spotted"){
+        score4++;
+    }
+
+    if (req.body.wq4 == "Knockout win"){
+        score4++;
+    }
+
+    if (req.body.wq5 == "Scorpion's grab move"){
+        score4++;
+    }
+
+    if (req.body.wq6 == "Portal gun sound"){
+        score4++;
+    }
+
+    if (req.body.wq7 == "Intervention shot"){
+        score4++;
+    }
+
+    if (req.body.wq8 == "You died"){
+        score4++;
+    }
+
+    if (req.body.wq9 == "Creeper hiss"){
+        score4++;
+    }
+
+    if (req.body.wq10 == "Wasted"){
+        score4++;
+    }
+
+    console.log(score4);
+    
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
